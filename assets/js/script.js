80661c266187e33d3b2cbfcb48446da67cf5b8d3
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-01-29 14:26:44 -0800

    dynamic bg color for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,24 @@ $(".description").on("click", function () {
   // highlight textarea
   $("#description").trigger("focus");
 });
+// get current day hour as a number
+var currentHour = moment().hour();
+// loop for each div with row class to get input and set day hour
+$('div.row').each((index, element) => {
+  // get ID from div.row
+  var rowId = element.id;
+  // get textarea el with id
+  var textareaEl = $('div#' + rowId + '> textarea');
+  // get only string number from id for each hour in the row
+  var hour = rowId.split('-')[1];
+  // set textarea background colors depending if current hour is past, now or present
+  if (hour) {
+    if (currentHour == hour) {
+      textareaEl.addClass("present")
+    } else if (currentHour < hour) {
+      textareaEl.addClass("future")
+    } else {
+      textareaEl.addClass("past")
+    }
+  }
+});
